620c7338b2115c5424fb35b6df955e83a49377b3
Sahil Gawade <104153594+Sahil-2005@users.noreply.github.com>
2025-12-16 13:15:12 +0530

    updated the API keys setting for gemini

--- gemini-clone/src/config/gemini.js
+++ gemini-clone/src/config/gemini.js
@@ -1,39 +1,35 @@
-const apikey = "AIzaSyArBihmgBEht11otzHhnRGs6yy3RDYSV_E";
-
+// No need to import dotenv in the frontend
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
 import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
-  } from "@google/generative-ai"
-  
-//   const apiKey = process.env.GEMINI_API_KEY;
-  const apiKey = "AIzaSyArBihmgBEht11otzHhnRGs6yy3RDYSV_E";
-  const genAI = new GoogleGenerativeAI(apiKey);
-  
-  const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-  });
-  
-  const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 40,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-  };
-  
-  async function run(prompt) {
-    const chatSession = model.startChat({
-      generationConfig,
-      history: [
-      ],
-    });
-  
-    const result = await chatSession.sendMessage(prompt);
-    console.log(result.response.text());
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
 
-    return result.response.text();
-  }
-  
-  export default run;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
